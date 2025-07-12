6581f2f48eeca3b71c8fac9d78501669ceb8abc1
lucas_demaimay <demaimay.lucas@gmail.com>
2025-07-12 23:49:51 +0200

    feat: add custom cursor

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,22 @@
 <script setup>
 import { RouterView } from "vue-router";
 import AppLayout from "./layouts/AppLayout.vue";
+import { ref } from "vue";
+
+const posX = ref(0);
+const posY = ref(0);
+
+function movePointer(event) {
+  posX.value = event.clientX;
+  posY.value = event.clientY;
+}
 </script>
 
 <template>
-  <AppLayout>
-    <RouterView />
-  </AppLayout>
+  <div @mousemove="movePointer" class="app">
+    <div :style="{ left: posX + 'px', top: posY + 'px' }" id="pointer"></div>
+    <AppLayout>
+      <RouterView />
+    </AppLayout>
+  </div>
 </template>
