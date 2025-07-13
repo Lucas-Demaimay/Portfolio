acb7858e383b047d07b2694e86b6c8188b301fe0
lucas_demaimay <demaimay.lucas@gmail.com>
2025-07-13 21:10:59 +0200

    fix: enhance pointer

--- src/App.vue
+++ src/App.vue
@@ -5,16 +5,22 @@ import { ref } from "vue";
 
 const posX = ref(0);
 const posY = ref(0);
+const pointer = ref(null);
 
 function movePointer(event) {
   posX.value = event.clientX;
   posY.value = event.clientY;
 }
+
 </script>
 
 <template>
-  <div @mousemove="movePointer" class="app">
-    <div :style="{ left: posX + 'px', top: posY + 'px' }" id="pointer"></div>
+  <div @pointermove="movePointer" class="app">
+    <div
+      ref="pointer"
+      :style="{ left: posX + 'px', top: posY + 'px' }"
+      id="pointer"
+    ></div>
     <AppLayout>
       <RouterView />
     </AppLayout>
